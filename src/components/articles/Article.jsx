f5863eed1bc3b5380e28101b1d7cd9a0bb5c6bc6
Elizabeth Eidelson <fluturecode@gmail.com>
2020-05-30 23:47:00 -0400

    updated version

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const 
- = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Article = () => {
+  return <div></div>;
+};
 
-export default 
-
+export default Article;
--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const 
- = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Articles = () => {
+  return <div></div>;
+};
 
-export default 
-
+export default Articles;
