34e7d49903f306b2f450ac22741a69aef25d2660
Elizabeth Eidelson <fluturecode@gmail.com>
2020-06-14 19:02:42 -0400

    finished front and back end

--- src/components/articles/ArticleForm.jsx
+++ src/components/articles/ArticleForm.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
 
-const ArticleForm = () => {
+const ArticleForm = ({ history }) => {
   const initialState = { title: "", text: "" };
   const [values, setValues] = useState(initialState);
-
   const handleSubmit = (e) => {
     // prevent HTML default submission
     e.preventDefault();
@@ -17,40 +16,42 @@ const ArticleForm = () => {
     })
       .then((response) => {
         if (response.ok) {
-          // let the user know that it went trough
           alert("Article successfully created");
-          // when we get confirmation we can reset the form to its original state (empty)
-          setValues(initialState);
+          return response.json().then((article) => {
+            history.push(`/articles/${article._id}`);
+          });
         }
       })
       .catch((error) => alert(error));
   };
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Title</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Title of your article..."
-          required={true}
-          onChange={(e) => setValues({ ...values, title: e.target.value })}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Text</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows="5"
-          placeholder="Text for your article..."
-          required={true}
-          onChange={(e) => setValues({ ...values, text: e.target.value })}
-        />
-      </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Title of your article..."
+            required={true}
+            onChange={(e) => setValues({ ...values, title: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Text</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows="5"
+            placeholder="Text for your article..."
+            required={true}
+            onChange={(e) => setValues({ ...values, text: e.target.value })}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 };
 
